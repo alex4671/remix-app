734e082530e53c34a840654aba3c49ae31fd9477
Alexey Kulchenko <al.kulchenko@gmail.com>
2022-08-11 00:28:15 +0300

    add error caught

--- app/root.tsx
+++ app/root.tsx
@@ -2,9 +2,10 @@ import {Container, MantineProvider} from "@mantine/core";
 import {StylesPlaceholder} from "@mantine/remix";
 import type {LoaderArgs, MetaFunction} from "@remix-run/node";
 import {json} from "@remix-run/node";
-import {Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration} from "@remix-run/react";
+import {Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useCatch} from "@remix-run/react";
 import {getUser} from "~/server/session.server";
 import {colors} from "~/utils/colors";
+import {ErrorPage} from "~/components/ErrorPage";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -42,6 +43,27 @@ export default function App() {
       <ScrollRestoration/>
       <Scripts/>
       <LiveReload/>
+      </body>
+      </html>
+    </MantineProvider>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  console.log("caught.statusText", caught);
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <html>
+      <head>
+        <title>Oops!</title>
+        <Meta/>
+        <Links/>
+        <StylesPlaceholder/>
+      </head>
+      <body>
+      <ErrorPage status={caught.status}/>
+      <Scripts/>
       </body>
       </html>
     </MantineProvider>
