8e5c58abcfb0a1558c921270d51ea904ac546c1b
Alexey Kulchenko <al.kulchenko@gmail.com>
2022-10-08 10:47:05 +0300

    Fix responsive

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,10 +1,8 @@
 import {Outlet} from "@remix-run/react";
-import {Box, Grid, Stack, Title} from "@mantine/core";
+import {Grid, Title} from "@mantine/core";
 import type {LoaderArgs, MetaFunction} from "@remix-run/node";
 import {json} from "@remix-run/node";
 import {requireUser} from "~/server/session.server";
-import {DesktopOnly} from "~/components/Utils/DesktopOnly";
-import {MobileOnly} from "~/components/Utils/MobileOnly";
 import {SettingsLinks} from "~/components/Settings/SettingsLinks";
 
 export const meta: MetaFunction = () => {
@@ -20,27 +18,18 @@ export async function loader({request}: LoaderArgs) {
 }
 
 export default function Settings() {
+
   return (
     <>
       <Title order={1} my={24}>Settings</Title>
-      <MobileOnly>
-        <Stack spacing={0}>
-          <Box mb={12}>
-            <SettingsLinks/>
-          </Box>
+      <Grid>
+        <Grid.Col sm={12} md={2}>
+          <SettingsLinks/>
+        </Grid.Col>
+        <Grid.Col sm={12} md={10}>
           <Outlet/>
-        </Stack>
-      </MobileOnly>
-      <DesktopOnly>
-        <Grid>
-          <Grid.Col span={2}>
-            <SettingsLinks/>
-          </Grid.Col>
-          <Grid.Col span={"auto"}>
-            <Outlet/>
-          </Grid.Col>
-        </Grid>
-      </DesktopOnly>
+        </Grid.Col>
+      </Grid>
     </>
   )
 }
