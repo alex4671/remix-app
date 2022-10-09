a0eb9f7da2d81bf2da8a6fbfb98f6c4800a7c1ca
Alexey Kulchenko <al.kulchenko@gmail.com>
2022-10-09 08:56:22 +0300

    Fixes

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,13 +1,11 @@
 import { RemixBrowser } from "@remix-run/react";
-import { ClientProvider } from "@mantine/remix";
+// import { ClientProvider } from "@mantine/remix";
 
 import { hydrateRoot } from "react-dom/client";
 
 hydrateRoot(
   document,
-  <ClientProvider>
-    <RemixBrowser />
-  </ClientProvider>
+  <RemixBrowser />
 );
 
 // import React, {startTransition} from "react";
