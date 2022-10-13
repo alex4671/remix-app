a38af213b52bf0043712c665c545d201f204285a
Alexey Kulchenko <al.kulchenko@gmail.com>
2022-10-13 22:40:51 +0300

    Fixes

---
+++ app/routes/payment/subscribed.tsx
@@ -0,0 +1,66 @@
+import {Button, Container, Group, Text, Title} from "@mantine/core";
+import {useFetcher, useNavigate} from "@remix-run/react";
+import type {LoaderArgs, MetaFunction} from "@remix-run/node";
+import {json} from "@remix-run/node";
+import {getUserPaymentStatus} from "~/models/user.server";
+import {useEffect} from "react";
+import {showNotification} from "@mantine/notifications";
+import {IconCheck} from "@tabler/icons";
+
+export const meta: MetaFunction = () => {
+  return {
+    title: "Subscribed"
+  };
+};
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const userPaymentStatus = await getUserPaymentStatus(request)
+
+  return json({userPaymentStatus})
+
+};
+
+export default function Subscribed() {
+  const statusFetcher = useFetcher();
+  const navigate = useNavigate()
+
+  const isLoading = statusFetcher.state === "loading"
+
+  useEffect(() => {
+    if (statusFetcher?.data?.userPaymentStatus === "active") {
+      console.log(statusFetcher?.data?.userPaymentStatus);
+      showNotification({
+        id: 'hello-there',
+        disallowClose: true,
+        autoClose: 3000,
+        onClose: () => navigate("/settings/pro"),
+        title: "You subscription confirmed",
+        message: 'You will redirected back in 3 second',
+        icon: <IconCheck size={16}/>,
+        color: "green",
+
+      })
+    }
+  }, [statusFetcher.data])
+
+
+  const handleCheckStatus = () => {
+    console.log("Checking status...");
+    statusFetcher.load("/payment/subscribed")
+  }
+
+  const handleGoHome = () => {
+    navigate('/')
+  }
+
+  return (
+    <Container size={600}>
+      <Title order={1}>You now on pro plan</Title>
+      <Text size={'lg'}>Processing could take couple minutes</Text>
+      <Group mt={24}>
+        <Button color={"emerald"} onClick={handleCheckStatus} loading={isLoading}>Check status</Button>
+        <Button color={"gray"} variant={"outline"} onClick={handleGoHome}>Go Home</Button>
+      </Group>
+    </Container>
+  )
+}
---
+++ app/routes/payment/unsubscribed.tsx
@@ -0,0 +1,28 @@
+import { Button, Container, Mark, Text, Title } from "@mantine/core";
+import { useLocation, useNavigate } from "@remix-run/react";
+import type { MetaFunction } from "@remix-run/node";
+
+
+export const meta: MetaFunction = () => {
+  return {
+    title: "Unsubscribed"
+  };
+};
+
+
+export default function Unsubscribed () {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const handleGoHome = () => {
+    navigate('/')
+  }
+
+  return (
+    <Container size={600}>
+      <Title order={1}>You successfully unsubscribed</Title>
+      <Text size={'lg'}>Processing could take couple minutes, you subscription is canceled and will be disabled <Mark color="green">{String(location.state)}</Mark></Text>
+      <Button mt={12} color={"gray"} variant={"outline"} onClick={handleGoHome}>Go Home</Button>
+    </Container>
+  )
+}
