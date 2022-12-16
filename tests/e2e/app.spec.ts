66cdbe7b9e911612dd7ed2802bbb3275531546f8
Alexey Kulchenko <al.kulchenko@gmail.com>
2022-12-16 22:54:45 +0300

    Fixes

--- tests/e2e/app.spec.ts
+++ tests/e2e/app.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test.describe('App', () => {
-	test('Should remain at "/".', async ({ page }) => {
+	test('Should redirect to "/login".', async ({ page }) => {
 		await page.goto('/');
 		await expect(page).toHaveURL('/login?redirectTo=%2F');
 	});
