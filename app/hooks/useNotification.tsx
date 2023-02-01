c97058e7c535b1c2f0d0c19a93d53ac267604581
Alexey Kulchenko <al.kulchenko@gmail.com>
2023-02-01 23:21:13 +0300

    Fixes

--- app/hooks/useNotification.tsx
+++ app/hooks/useNotification.tsx
@@ -1,23 +1,20 @@
 import { showNotification } from '@mantine/notifications';
 import { useFetchers } from '@remix-run/react';
 import { IconCheck, IconX } from '@tabler/icons-react';
-import { useEffect } from 'react';
 
 export const useNotification = () => {
 	const fetchers = useFetchers();
 	const activeData = fetchers.find((f) => f.state === 'loading')?.data;
 
-	useEffect(() => {
-		if (activeData?.intent) {
-			showNotification({
-				title: activeData?.message,
-				message: undefined,
-				color: activeData?.success ? 'green' : 'red',
-				autoClose: 2000,
-				icon: activeData?.success ? <IconCheck /> : <IconX />,
-			});
-		}
-	}, [activeData]);
+	if (activeData?.intent) {
+		showNotification({
+			title: activeData?.message,
+			message: undefined,
+			color: activeData?.success ? 'green' : 'red',
+			autoClose: 2000,
+			icon: activeData?.success ? <IconCheck /> : <IconX />,
+		});
+	}
 
 	return null;
 };
